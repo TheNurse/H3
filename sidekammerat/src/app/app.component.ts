5f14c2b4268b918f552e91c432dfda13092b1190
TheNurse <jonas-iversen@live.dk>
2020-08-10 12:19:45 +0200

    added Din sidekammarat assignment.

---
+++ sidekammerat/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Min Sidemand!';
+  name = 'Casper DickCity Miller GenericDanishLastname';
+  imageUrl = 'https://scontent-cph2-1.xx.fbcdn.net/v/t31.0-8/614643_517945358230008_1701467435_o.jpg?_nc_cat=107&_nc_sid=de6eea&_nc_ohc=AcK8wvxLkIkAX9jlDy4&_nc_ht=scontent-cph2-1.xx&oh=d8e9fb55f8f55124a65cb1f13ade7914&oe=5F567A3D';
+  description = 'Casper skal huske at lave jokes hverdag!'
+}
